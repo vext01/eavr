8d335b86a059a85955b99d62fbfcb6b12c7349a4
eb771 <eb771@4573b9c3-91d8-c8a9-e57d-f710cb21d9de>
2010-12-29 19:05:28 +0000

    commit split out structure
    git-svn-id: svn+ssh://myrtle.kent.ac.uk/home/cug/eb771/svn/eavr/trunk@12 4573b9c3-91d8-c8a9-e57d-f710cb21d9de

--- lcd/lcd.c
+++ lcd/lcd.c
@@ -13,9 +13,6 @@
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- * A 4-bit HD44780 interface for an AVR board.
- * Currently does not check busy status of the controller, only bitbangs.
  */
 
 #include <avr/io.h>
@@ -23,31 +20,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* command "bases", commands before any params set */
-#define LCD_CLEAR_BASE		(1 << 0)
-#define LCD_HOME_BASE		(1 << 1)
-#define LCD_DC_BASE		(1 << 3)
-#define LCD_FS_BASE		(1 << 5)
-
-/* non data pins */
-#define LCD_P_EN		(1 << 0)
-#define LCD_P_RW		(1 << 1)
-#define LCD_P_RS		(1 << 2)
-
-/* function set stuff */
-#define LCD_P_FS_DL		(1 << 4)
-#define LCD_P_FS_N		(1 << 3)
-#define LCD_P_FS_F		(1 << 2)
-
-/* display control stuff */
-#define LCD_P_DC_D		(1 << 2)
-#define LCD_P_DC_C		(1 << 1)
-#define LCD_P_DC_B		(1 << 0)
-
-/* bit bang time allowances */
-#define LCD_DELAY_INIT		15
-#define LCD_DELAY_CMD		5
-#define LCD_DELAY_DATA		1
+#include "lcdparams.h"
+#include "ealcd.h"
 
 /*
  * XXX make operations non-blatting, ie use the current values of the
@@ -57,23 +31,7 @@
  * Also currently assumed that the 4th bit of the control bus is the
  * LCD logic power. We hold this low and bring it up after so that the
  * arduino initialisation does not interfere with the LCD configuration.
- *
  */
-
-/* which ports is your shit on? */
-#define	LCD_CTRL		PORTB
-#define LCD_DATA		PORTC
-#define LCD_DBUS_SHIFT		0
-#define LCD_CBUS_SHIFT		0
-
-/* check the user defined all the stuff we needed */
-#if (!defined LCD_DATA) || !defined(LCD_CTRL)
-#error "Please define LCD_DATA and LCD_CTRL"
-#endif
-
-#if (!defined LCD_DBUS_SHIFT) || !defined(LCD_CBUS_SHIFT)
-#error "Please define LCD_CBUS_SHIFT and LCD_DBUS_SHIFT"
-#endif
 
 void
 lcd_init()
@@ -84,11 +42,10 @@ lcd_init()
 }
 
 /* write in 4 byte mode (LSb) */
-/* don't call this yourself */
 void
 lcd_write4(uint8_t rs, uint8_t rw, uint8_t data)
 {
-	uint8_t			ctrl = 8; /* XXX to keep lcd power on */
+	uint8_t			ctrl = 8; /* LCD_CTRL << 4 == lcd power */
 
 	if (rw)
 		ctrl += LCD_P_RW;
@@ -105,7 +62,6 @@ lcd_write4(uint8_t rs, uint8_t rw, uint8_t data)
 	LCD_CTRL = ctrl | LCD_P_EN;
 	_delay_ms(LCD_DELAY_CMD);
 	LCD_CTRL = ctrl & ((~LCD_P_EN) & 0x0f);
-
 }
 
 void
@@ -193,52 +149,4 @@ void
 lcd_home()
 {
 	lcd_write8(0, 0, LCD_HOME_BASE);
-}
-
-void
-debug_blink()
-{
-	return;
-	PORTC = 0xff;
-	_delay_ms(500);
-	PORTC = 0x0;
-}
-
-int
-main(void)
-{
-	char			rev[20] = "$Revision$", *c;
-
-	/* set PORTB for LCD databus */
-	DDRB = 0x0f;
-
-	/* PORTC for LCD control bus */
-	DDRC = 0x0f;
-
-	LCD_DATA = 0;
-	LCD_CTRL = 0;
-
-	/* set up lcd */
-	lcd_init();
-
-	/* set number of lines */
-	lcd_function_set(2);
-
-	/* cursor on, blink, font 1 */
-	lcd_display_ctrl(1, 1, 1);
-
-	/* clear screen and start at far left */
-	lcd_home();
-	lcd_clear();
-
-	lcd_put_string("EAVR LCD - lcd.c                        ");
-
-	lcd_put_string("Version");
-	for (c = rev; *c != ':'; c ++);
-	for (; *c != '$'; c ++)
-		lcd_put_char(*c);
-
-	while(1);
-
-	return (0);
 }
